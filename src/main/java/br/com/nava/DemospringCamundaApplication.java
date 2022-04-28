df0bc609ed36cb51e8344b00db1562cf0e308e3b
Rachel de Araujo Rosa Ferreira <rachel.ferreira@nava.com.br>
2022-04-28 17:00:58 -0300

    first commit camunda

---
+++ src/main/java/br/com/nava/DemospringCamundaApplication.java
@@ -0,0 +1,13 @@
+package br.com.nava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringCamundaApplication.class, args);
+	}
+
+}
